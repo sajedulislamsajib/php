f0a56d6479a37f47d2f9a300ce43f26fb8d9f34e
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-11-07 10:08:05 +0600

    variable name convention

--- test.php
+++ test.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello World sajib!";
+//variable
+$name = "Bangladesh";
+$age = 25;
+$myname = "Hasin Hyder";
+
+$my_name = '';//undercase => follow in wordpress
+$myNameIs = '';//camel case
+$MyNameIs= '';//Title case
