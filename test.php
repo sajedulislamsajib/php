5351a6f05c619b786e0b561f56dee872aa8f76c9
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-11-30 08:43:07 +0600

    php contant, variable

--- test.php
+++ test.php
@@ -1,9 +1,13 @@
 <?php
 //variable
-$name = "Bangladesh";
-$age = 25;
-$myname = "Hasin Hyder";
+// $name = "Bangladesh";
+// $age = 25;
+// $myname = "Hasin Hyder";
 
-$my_name = '';//undercase => follow in wordpress
-$myNameIs = '';//camel case
-$MyNameIs= '';//Title case
+// $my_name = '';//undercase => follow in wordpress
+// $myNameIs = '';//camel case
+// $MyNameIs= '';//Title case
+
+
+define('PI',3.14159);
+echo "vaue of pi = {$PI}".PI;
