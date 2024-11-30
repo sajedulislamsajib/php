de27c3d8828ac74da3e53e50f86eee569fb64567
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-11-30 10:58:00 +0600

    post fix and pre fix working machanism in php

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
 <?php
-
-$x =$y =2;
-echo $x,"\n",$y;
+$number = 7;
+$m = $number++;
+echo $m,"\n", $number,"\n";
+// $m=$n;
+// $n=$n+1;
+$s = 7;
+$t = ++$s;
+echo $t,"\n",$s;
+// $t = 1+ $n;
+// $s = $t + 1;
