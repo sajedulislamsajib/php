f2a6dbff07ec048a80bbcffd0a700ba6de409371
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-11-30 12:55:01 +0600

    decimal to binary,octal, hexadecimal in php

--- test.php
+++ test.php
@@ -1,11 +1,9 @@
 <?php
-$number = 7;
-$m = $number++;
-echo $m,"\n", $number,"\n";
-// $m=$n;
-// $n=$n+1;
-$s = 7;
-$t = ++$s;
-echo $t,"\n",$s;
-// $t = 1+ $n;
-// $s = $t + 1;
+$number = 123;
+$o = 012;//for octal use 0 (zero) in the first 
+$h = 0xf;// for hexadecimal use 0x in the first 
+
+//printf("The Number is %d and %d , %d", $n, $o,$h);
+printf("The binary equivalent of %d is %b",1212,1212);
+printf("The octal equivalent of %d is %o",1212,1212);
+printf("The hexadecimal equivalent of %d is %x",1212,1212);
