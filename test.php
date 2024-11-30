09e28bb3139bbd9b622006abe33c6ae8b8e5692d
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-11-30 08:51:59 +0600

    we use var_dump for information about the variable.It gives variable type. length of varible

--- test.php
+++ test.php
@@ -1,13 +1,6 @@
 <?php
-//variable
-// $name = "Bangladesh";
-// $age = 25;
-// $myname = "Hasin Hyder";
-
-// $my_name = '';//undercase => follow in wordpress
-// $myNameIs = '';//camel case
-// $MyNameIs= '';//Title case
-
-
-define('PI',3.14159);
-echo "vaue of pi = {$PI}".PI;
+echo 'Hello';
+echo "\n";
+echo 123;
+$name = "Earth";
+var_dump($name);
